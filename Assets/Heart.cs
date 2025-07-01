54cef2241cbe230b56d6311fae9eaee17c4a052f
Pendrill <amw694@nyu.edu>
2025-07-01 19:52:56 -0400

    init

---
+++ Assets/Heart.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Heart : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseOver()
+    {
+        Debug.Log("mouse is over the object");
+    }
+
+    void CheckHeartClicked()
+    {
+      
+    }
+
+    void HeartClicked()
+    {
+
+    }
+}
